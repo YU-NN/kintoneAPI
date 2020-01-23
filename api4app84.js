4f56006f174a554a1fb5eca925600b4f90c55176
YU-NN <yoshi_nn_ihsoy@icloud.com>
2020-01-23 16:15:53 +0900

    今月分のレコードを取得するクエリを追加

--- api4app84.js
+++ api4app84.js
@@ -16,13 +16,14 @@
 
           //日報アプリから、特定の店舗についてのレコードを取得するクエリ(今回はstore_recordsの一番最初の店舗についてのレコードを取得)
           //ここについてfor文で回す。
-          var nippou_query = "店舗ID = "+ store_records[0]["id"]["value"];
+          var nippou_this_month_query = "作成日時 = THIS_MONTH() and 店舗ID = "+ store_records[0]["id"]["value"];
+          //var nippou_this_month_query = "作成日時 = TODAY() and 店舗ID = "+ store_records[0]["id"]["value"];
           //日報アプリから、特定の店舗の"店舗ID","リード合計","来店数","販売台数”を取得するJSON
           //加えてやること：今月分のレコードを取得、先月分のレコードを取得
           var nippou_body = {
             "app": 83,
             "fields": ["店舗ID","店舗名","リード合計","来店数","販売台数"],
-            "query": nippou_query
+            "query": nippou_this_month_query
           };
 
 
@@ -32,9 +33,11 @@
               // success
               //この中でnippou_recordsのサイズ分のforループを回す。
               nippou_records = nippou_resp["records"];
+
               var lead_sum      = 0;
               var guest_sum     = 0;
               var saled_car_sum = 0;
+
               for (var i = 0; i < nippou_records.length; i++) {
                 lead_sum      += Number(nippou_records[i]["リード合計"]["value"]);
                 guest_sum     += Number(nippou_records[i]["来店数"]["value"]);
