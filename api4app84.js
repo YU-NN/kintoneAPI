11d76d156d9af6d6ddbdae66a9b2d2c6c37b3c56
YU-NN <yoshi_nn_ihsoy@icloud.com>
2020-01-23 15:27:35 +0900

    詳細コメントを追加

--- api4app84.js
+++ api4app84.js
@@ -1,41 +1,36 @@
-
 (function() {
     "use strict";
 
 
-
-
     kintone.events.on('app.record.index.show', function(event) {
-
       //全店舗のIDと店舗名を取得するJSON
       var store_body = {
           "app": 57,
           "fields": ["id","name"]
       };
-
       var store_records = {};
       kintone.api(kintone.api.url('/k/v1/records', true), 'GET', store_body, function(store_resp) {
           // success
+          //存在する店舗すべてを取得し、sotre_recordsに保存
           store_records = store_resp["records"];
 
-
-
-
-
-
-
-          var str = "店舗ID = "+ store_records[0]["id"]["value"];
-
+          //日報アプリから、特定の店舗についてのレコードを取得するクエリ(今回はstore_recordsの一番最初の店舗についてのレコードを取得)
+          //ここについてfor文で回す。
+          var nippou_query = "店舗ID = "+ store_records[0]["id"]["value"];
+          //日報アプリから、特定の店舗の"店舗ID","リード合計","来店数","販売台数”を取得するJSON
           var nippou_body = {
             "app": 83,
             "fields": ["店舗ID","リード合計","来店数","販売台数"],
-            "query": str
+            "query": nippou_query
           };
+
+
 
           var nippou_records = {};
           kintone.api(kintone.api.url('/k/v1/records', true), 'GET', nippou_body, function(nippou_resp) {
               // success
-              alert(nippou_resp["records"][0]["店舗ID"]["value"]);
+              nippou_records = nippou_resp["records"];
+              alert(nippou_records[0]["店舗ID"]["value"]);
           }, function(error) {
               // error
               alert("error");
