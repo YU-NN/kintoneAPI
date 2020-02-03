a137c6b6f8f9714fa6ea3692bf92cd4ea329c27e
YU-NN <yoshi_nn_ihsoy@icloud.com>
2020-02-03 18:54:47 +0900

    店舗名も入れてループ。しかし日報レコードでフラグを建てるのではなく、月間レコードでフラグを建てることを忘れていた。

--- test4putpost.js
+++ test4putpost.js
@@ -3,7 +3,7 @@ var now   = new Date();
 var year  = now.getFullYear();
 var month = now.getMonth()+1;
 
-var boolIsAlreadyExist = false;
+
 var storeMonthlyRecordId = 0;
 
 
@@ -61,34 +61,55 @@ var store_data = {};
     "use strict";
 
 
-
-
-
-
-
     //ここで同期処理を行う。
     var handler = function(event) {
+
+
+
 
       return kintone.api(kintone.api.url('/k/v1/records', true), 'GET', store_body).then(function(get_store_resp){
         store_data = get_store_resp["records"];
         return kintone.api(kintone.api.url('/k/v1/records', true), 'GET',  records4get);
       }).then(function(get_nippou_resp){
+
+
+
+
+
+
         //処理
-        for (var i = 0; i < get_nippou_resp["records"].length; i++) {
-          if (get_nippou_resp["records"][i]["作成日時"]["value"].substr(0,7) == "2020-02" && get_nippou_resp["records"][i]["店舗名"]["value"] == "杉並") {
-            boolIsAlreadyExist = true;
-            record4put["id"] = Number(get_nippou_resp["records"][i]["レコード番号"]["value"]);
-            break;
+        for (var i = 0; i < store_data.length; i++) {
+          //この店舗の月間レコードは無いと仮定。
+          var boolIsAlreadyExist = false;
+          //record4putとrecord4postをiの店に合うように変更。
+          record4post["店舗名"]["value"] = store_data[i]["name"]["value"];
+
+
+          for (var j = 0; j < get_nippou_resp["records"].length; j++) {
+
+
+            if (get_nippou_resp["records"][j]["作成日時"]["value"].substr(0,7) == "2020-02" && get_nippou_resp["records"][j]["店舗名"]["value"] == store_data[i]["name"]["value"]) {
+              boolIsAlreadyExist = true;
+              record4put["id"] = Number(get_nippou_resp["records"][j]["レコード番号"]["value"]);
+              break;
+            }
+          }
+
+          if (boolIsAlreadyExist) {
+            //更新用のJSONにPUS
+            records4put["records"].push(record4put);
+          }else {
+            //作成用のJSONにPUSH
+            records4post["records"].push(record4post);
           }
         }
 
-        if (boolIsAlreadyExist) {
-          //更新用のJSONにPUS
-          records4put["records"].push(record4put);
-        }else {
-          //作成用のJSONにPUSH
-          records4post["records"].push(record4post);
-        }
+
+
+
+
+
+
 
 
 
